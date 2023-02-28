e4a29806f8c1de9c86cbc496f5532eada1c9d2d4
Paulo Roberto Campos de Vasconcelos <paulorcv@gmail.com>
2023-02-28 08:47:43 -0300

    Refactor: retirando métodos setters dos campos obrigatórios do Author e deixando somente no construtor

--- src/main/java/br/com/epermatozoideguerreiro/cdc/author/Author.java
+++ src/main/java/br/com/epermatozoideguerreiro/cdc/author/Author.java
@@ -41,24 +41,12 @@ public class Author {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getDescription() {
         return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     @Override
