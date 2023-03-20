e9333c1124dec11d0dfde7e3e0a8fbc835d00ed5
Paulo Roberto Campos de Vasconcelos <paulorcv@gmail.com>
2023-03-20 17:47:16 -0300

    Refactor: trocando o error handling dos controller por um Controller Advice

--- src/main/java/br/com/epermatozoideguerreiro/cdc/author/AuthorController.java
+++ src/main/java/br/com/epermatozoideguerreiro/cdc/author/AuthorController.java
@@ -34,16 +34,4 @@ public class AuthorController {
         authorRepository.save(request.toModel());
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(
-            MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return errors;
-    }
 }
--- src/main/java/br/com/epermatozoideguerreiro/cdc/category/CategoryController.java
+++ src/main/java/br/com/epermatozoideguerreiro/cdc/category/CategoryController.java
@@ -27,16 +27,5 @@ public class CategoryController {
     public void create(@Valid @RequestBody NewCategoryRequest request) {
         categoryRepository.save(request.toModel());
     }
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(
-            MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return errors;
-    }
+
 }
